523eafc03bc6a833bb013f99d5f3b3cea60ffa19
Lailson de Lima <lailsonlm@hotmail.com>
2021-05-15 11:56:25 -0300

    snake movements

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
+// Criação do BG
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
-let box = 32;
+let box = 16;
 
+// Criação da Cobrinha
 let snake = [];
 snake[0] = {
-    x: 8 * box,
-    y: 8 * box
+    x: 16 * box,
+    y: 16 * box
 }
+
+// Direção
+let direction = "right"
+
 
 function createBG() {
     context.fillStyle = "lightgreen";
-    context.fillRect(0, 0, 16 * box, 16 * box);
+    context.fillRect(0, 0, 32 * box, 32 * box);
 }
 
 function createSnake() {
@@ -20,5 +26,38 @@ function createSnake() {
     }
 }
 
-createBG()
-createSnake()
+function startGame() {
+    createBG()
+    createSnake()
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") {
+        snakeX += box;
+    } else if(direction == "left") {
+        snakeX -= box;
+    } else if(direction == "up") {
+        snakeY -= box;
+    } else if(direction == "down") {
+        snakeY += box;
+    }
+
+    //"Fazer a cobrinha andar"
+    
+    // Remover o último elemento do array
+    snake.pop();
+
+    // Adiciona um elemento no início do array
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
+
+// Rodar a função startGame a cada 100ms
+let game = setInterval(startGame, 100)
+
+
